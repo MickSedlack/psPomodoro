2f9ad2bc6684e03efb5b3cf729db01d5f220cd94
MickSedlack <MickSedlack@gmail.com>
2026-05-08 14:31:07 -0400

    Change fonts for label and ticker.
    Different locations and fonts for ticker, and status label. Removal of whitespace between variables and values.

--- pomodoro.ps1
+++ pomodoro.ps1
@@ -15,33 +15,34 @@ $null = $PInvoke::SetProcessDpiAwareness(2)
 Add-Type -AssemblyName PresentationCore 
 $Form = New-Object system.Windows.Forms.Form
 
-$Form                            		= New-Object system.Windows.Forms.Form
-$Form.ClientSize                	= '275,175'
-$Form.text							= "Pomodoro"
-$Form.BackColor                	= "#f7f5dd"
-$Form.TopMost                  	= $false
+$Form = New-Object system.Windows.Forms.Form
+$Form.ClientSize = '230,175'
+$Form.text = "Pomodoro"
+$Form.BackColor = "#f7f5dd"
+$Form.TopMost = $false
+$Form.StartPosition = [FormStartPosition]::CenterScreen;
 
-$CLock                          		= New-Object system.Windows.Forms.Label
-$CLock.text 						= "The time is?"
-$CLock.BackColor				= "#f7f5dd"
-$CLock.AutoSize					= $true
-$CLock.width						= 25
-$CLock.height						= 10
-$CLock.location					= New-Object System.Drawing.Point(85,50)
-$CLock.Font                      	= 'Microsoft Sans Serif,28'
-$CLock.ForeColor				= "#ff0000"
+$CLock = New-Object system.Windows.Forms.Label
+$CLock.text = "The time is?"
+$CLock.BackColor = "#f7f5dd"
+$CLock.AutoSize = $true
+$CLock.width = 25
+$CLock.height = 10
+$CLock.location = New-Object System.Drawing.Point(55,50)
+$CLock.Font = 'Overpass,28'
+$CLock.ForeColor = "#ff0000"
 
-$label 								= New-Object System.Windows.Forms.Label
-$label.Location				 	= New-Object System.Drawing.Point(60,0)
-$label.Size 							= New-Object System.Drawing.Size(280,40)
-$label.Font                      	= 'Microsoft Sans Serif,24'
-#$label.Text 						= "Pomodoro"
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(60,0)
+$label.Size = New-Object System.Drawing.Size(280,40)
+$label.Font = 'Overpass,24'
+#$label.Text = "Pomodoro"
 
-$ticker 								= New-Object System.Windows.Forms.Label
-$ticker.Location				 	= New-Object System.Drawing.Point(20,20)
-$ticker.Size 						= New-Object System.Drawing.Size(280,40)
-$ticker.Font                      	= 'Microsoft Sans Serif,24'
-$ticker.text							= "a"
+$ticker = New-Object System.Windows.Forms.Label
+$ticker.Location = New-Object System.Drawing.Point(90,100)
+$ticker.Size = New-Object System.Drawing.Size(280,40)
+$ticker.Font = [Font]::new("Segoe UI", 24)
+$ticker.text = ""
 
 $pauseButton = New-Object System.Windows.Forms.Button
 $pauseButton.Location = New-Object System.Drawing.Point(100,150)
